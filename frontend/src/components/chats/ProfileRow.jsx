3cc121b9c13a7cfaeeaeb24808cf85b431f49a82
nemoguigrat <sharos185@gmail.com>
2022-06-02 16:21:23 +0500

    load images from frontend

--- frontend/src/components/chats/ProfileRow.jsx
+++ frontend/src/components/chats/ProfileRow.jsx
@@ -3,15 +3,43 @@ import { Context } from "../Context"
 import axios from "axios";
 import { URL } from "../../const"
 import '../../styles/style.css'
+import Modal from "react-modal";
 
 const ProfileRow = (props) => {
   const [token, setToken] = useContext(Context);
+  const [isOpen, setIsOpen] = useState(false);
+  const [image, setImage] = useState(null);
+
+  const handleViewModal = () => setIsOpen(!isOpen);
+
+  const uploadImage = () => {
+      if (image != null) {
+          let formData = new FormData();
+          formData.append("file", image);
+          axios({
+              method: "post",
+              url: URL + "api/user/upload",
+              data: formData,
+              headers: { "Content-Type": "multipart/form-data",
+                  "Authorization": 'Bearer ' + token},
+          }).then(() => console.log("success!"))
+      }
+  }
 
   return (
     <div className='chatRow profileBorder'>
-        {props.user.image != null ? <img className='chatImage' src={props.user.image.filename} alt="" /> :
-            <div className='chatImage chatImagePlaceholder'/>}
+        {props.user.image != null ? <img className='chatImage' src={props.user.image.filename} onClick={handleViewModal}/> :
+            <div className='chatImage chatImagePlaceholder' onClick={handleViewModal}/>}
       <p className='rowText'>{props.user.firstName} {props.user.secondName}</p>
+        <Modal isOpen={isOpen} appElement={this}>
+            <h1>Загрузить изображение</h1>
+            <form>
+                <input type="file" onChange={(event) => setImage(event.target.files[0])}/>
+                <input type="submit" onClick={uploadImage}/>
+            </form>
+
+            <button onClick={handleViewModal}>Закрыть окно</button>
+        </Modal>
     </div>
   )
 }
--- frontend/src/components/messages/InfoRow.jsx
+++ frontend/src/components/messages/InfoRow.jsx
@@ -1,12 +1,46 @@
-import React from 'react'
+import React, {useState, useContext} from 'react'
 import '../../styles/style.css'
+import Modal from "react-modal";
+import axios from "axios";
+import { URL } from "../../const";
+import {Context} from "../Context";
 
 const InfoRow = (props) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [token, setToken] = useContext(Context);
+    const [image, setImage] = useState(null);
+
+    const handleViewModal = () => setIsOpen(!isOpen);
+
+    const uploadImage = () => {
+        console.log(props.currentRoom.id)
+        if (image != null) {
+            let formData = new FormData();
+            formData.append("file", image);
+            axios({
+                method: "post",
+                url: URL + "api/chat/room/" + props.currentRoom.id + "/upload",
+                data: formData,
+                headers: { "Content-Type": "multipart/form-data",
+                    "Authorization": 'Bearer ' + token},
+            }).then(() => console.log("success!"))
+        }
+    }
+    
   return (
     <div className='infoRow interface profileBorder'>
-        {props.currentRoom.image != null ? <img className='chatImage' src={props.currentRoom.image.filename} alt="" /> :
-            <div className='chatImage chatImagePlaceholder' />}
+        {props.currentRoom.image != null ? <img className='chatImage' src={props.currentRoom.image.filename} onClick={handleViewModal}/> :
+            <div className='chatImage chatImagePlaceholder' onClick={handleViewModal}/>}
       <div className='rowText'>{props.currentRoom.name}</div>
+      <Modal isOpen={isOpen} appElement={this}>
+          <h1>Загрузить изображение</h1>
+          <form>
+              <input type="file" onChange={(event) => setImage(event.target.files[0])}/>
+              <input type="submit" onClick={uploadImage}/>
+          </form>
+
+          <button onClick={handleViewModal}>Закрыть окно</button>
+      </Modal>
     </div>
   )
 }
